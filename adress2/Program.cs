118295a9ae375357f50136fd320136f88cc552cc
evilsun77 <therealsky2032@gmail.com>
2021-04-20 08:52:50 +0300

    Edit Program file And Class File!

--- adress2/Program.cs
+++ adress2/Program.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 
-namespace adress2
+namespace adress
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            adress adress1 = new adress();
+            adress1.CITY = Polotsk;
+            adress1.STREET = Oktabrkskaya street;
+            adress1.NUMBER = 49;
+            adress1.ShowCity();
+            adress1.ShowStreet();
+            adress1.ShowNumber();
+
+        }
     }
 }
