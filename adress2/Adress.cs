2e8164fe759140894e827ce64817b5e3c86e0299
evilsun77 <therealsky2032@gmail.com>
2021-04-20 08:52:05 +0300

    Edit Adress Class!

--- adress2/Adress.cs
+++ adress2/Adress.cs
@@ -7,8 +7,62 @@ namespace adress
 {
     class adress
     {
-        string city;
-        string street;
-        int number;
+        public string city;
+        public string street;
+        public int number;
+        public adress()
+        {
+            city = "";
+            street = "";
+            number = 0;
+        }
+        public void ShowCity()
+        {
+            Console.WriteLine("The current adress in {0}", city);
+        }
+        public void ShowStreet()
+        {
+            Console.WriteLine("The current adress on {0}", street);
+        }
+        public void ShowNumber()
+        {
+            Console.WriteLine("The current number of house is {0}", number);
+        }
+        public string CITY
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string STREET
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+            }
+        }
+        public int NUMBER
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+
+        }
     }
+
 }
+
